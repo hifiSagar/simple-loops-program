18f6961192cc4ebaa79fe44c1ce1d684fbaee3b8
SAGAR T <ssagu6661@gmail.com>
2024-08-20 11:42:41 -0400

    Add files via upload

---
+++ c16.py
@@ -0,0 +1,33 @@
+# LOOPS
+# # while loop
+
+# i = 0
+# while (i<10):
+#     print("hello world")
+#     i += 1
+
+# # FOR LOOP
+#  #  ANOTHER PROGRAM
+# for i in range (10):
+#     print("hello world")
+
+# # ANOTHER PROGRAM
+# for i in range(21):
+#     print(i)
+  
+    
+# ANOTHER PROGRAM
+for num in range(20):
+    if num % 3 == 0:
+        print(num)
+    elif num == 17:
+        print(num) 
+     
+
+# ANOTHER PROGRAM
+
+for num in range(20):
+    if num % 3 == 0:
+        print(num)
+    
+
